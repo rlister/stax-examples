80d814fd54dce796916e786e1205a02d367525f0
Pablo Cantero <pablohstc@gmail.com>
2018-06-20 13:46:37 -0400

    MOAR updating

--- lib/generators/s3/s3_generator.rb
+++ lib/generators/s3/s3_generator.rb
@@ -1,9 +1,31 @@
 module Stax
   module Generators
     class S3Generator < Base
-      desc 's3 generator.'
+      desc 'Create basic S3 bucket'
+
+      class_option :stack, type: :string, default: nil, desc: 'stack to install'
+      class_option :bucket_name, type: :string, default: nil, desc: 'bucket_name'
 
       source_root File.expand_path('templates', __dir__)
+
+      attr_accessor :stack, :bucket_name
+
+      def add_to_staxfile
+        append_to_file 'Staxfile', "stack :#{stack}\n"
+      end
+
+      def check_args
+        usage! unless args.empty?
+      end
+
+      def ask_for_options
+        @stack       = options[:stack] || ask('stack to use or create?', default: 's3')
+        @bucket_name = options[:bucket_name]
+      end
+
+      def add_templates
+        directory '.', 'cf'
+      end
 
       # see https://www.rubydoc.info/github/wycats/thor/Thor/Actions for methods
 
@@ -25,11 +47,11 @@ module Stax
       #   empty_directory(path)
       # end
 
-      # private
+      private
 
-      # def self.banner(*args)
-      #   "#{basename} generate #{command_name} ARG"
-      # end
+      def self.banner(*args)
+        "#{basename} generate #{command_name} ARG"
+      end
     end
   end
 end
